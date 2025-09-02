41619a9153a766549ce9a54cc1038fe62ff27726
Kbizziiey <karabomakganyoga2@gmail.com>
2025-09-02 21:22:57 +0200

    modified ny props component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,8 +1,9 @@
 import WelcomeMessage from "./components/WelcomeMessage"
-import Header from "./Header"
-import MainContent from "./MainContent"
-import Footer from "./Footer"
+import Header from "./components/Header"
+import MainContent from "./components/MainContent"
+import Footer from "./components/Footer"
 import UserProfile from "./components/UserProfile"
+import { useState } from "react";
 
 function App() {
   return (
