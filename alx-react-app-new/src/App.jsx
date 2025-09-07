dedc043eb0f9a045a4a9fd0b127604d0ead0a15a
Kbizziiey <karabomakganyoga2@gmail.com>
2025-09-07 13:55:26 +0200

    styling react components

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,21 @@
+import WelcomeMessage from "./components/WelcomeMessage"
+import Header from "./components/Header"
+import MainContent from "./components/MainContent"
+import Footer from "./components/Footer"
+import UserProfile from "./components/UserProfile"
+import Counter from "./components/Counter"
+
+function App() {
+  return (
+    <>
+      {/*<WelcomeMessage />
+      <Header /> 
+      <MainContent />
+      <Footer />
+      <UserProfile name="Karabo" age="23" bio="Software developer" />*/}
+      <Counter />
+    </>
+  )
+}
+
+export default App
