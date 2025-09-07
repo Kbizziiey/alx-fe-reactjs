a2774d7dc6b7771e10b9cf45755a3db19eacd9b6
Kbizziiey <karabomakganyoga2@gmail.com>
2025-09-07 15:17:44 +0200

    updated the Navbar

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,3 +1,4 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 import "./App.css";
 import About from "./components/About";
 import Contact from "./components/Contact";
@@ -5,14 +6,22 @@ import Home from "./components/Home";
 import Services from "./components/Services";
 
 function App() {
+ 
   return (
-    <>
-      <Home />
-      <About />
-      <Services />
-      <Contact />
+    <>  
+    <BrowserRouter>
+    <Navbar/>
+    <Routes> 
+      <Route path='./' element={<Home/>}/> 
+      <Route path='./about' element={<About/>}/>  
+      <Route path='./contact' element={<Contact/>}/> 
+      <Route path='./services' element={<Services/>}/>
+
+    </Routes> 
+    </BrowserRouter>
     </>
-  );
+  )
+   
 }
 
-export default App;
+export default App
