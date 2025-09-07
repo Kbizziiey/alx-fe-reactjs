9f81db951ed633b617b371f3a3ec9f0c92d0f175
Kbizziiey <karabomakganyoga2@gmail.com>
2025-09-07 14:43:52 +0200

    simple company website

---
+++ my-company/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import Services from "./components/Services";
+
+function App() {
+  return (
+    <>
+      <Home />
+      <About />
+      <Services />
+      <Contact />
+    </>
+  );
+}
+
+export default App;
